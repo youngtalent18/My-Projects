d5afc03af2de825785610dc92486bccc7a5ab0f8
Stephen Anti <stephenanti63@gmail.com>
2025-07-09 23:04:00 -0700

    Madlibs Game.py(Beginner0

---
+++ Madlibs Game.py
@@ -0,0 +1,18 @@
+#-----Welcome to Madlibs Game-----
+
+#---In here I write about myself with madlibs game.
+proper_noun = input('Enter a proper noun: ')
+age = input('Enter your age: ')
+work = input('What do you do: ')
+institution = input('Enter an institution: ')
+course = input('Enter a course of your choice: ')
+occupation = input('Enter an occupation: ')
+software_programme = input('Enter a software programme you like: ')
+print(f'My name is {proper_noun}. I am {age} years old and a/an {work} at {institution},')
+print(f'I am currently studying one of the worlds leading courses namely {course}.')
+print(f'I dream of becoming a professional {occupation} in the world, in order to achieve that I study {software_programme} as much as I can. '
+      f'We live I a world of technology now so I have to know my stand whether to go into tech or something else... but '
+      f'I think I will stick to tech industries and some freelance jobs and content creation staff. Why tech? many may ask.'
+      f'Now! here is two reasons with which I chose tech.'
+      f'(1)It fetch alot in terms of money (2)We are in a tech world and stuffs as such are in high demand.'
+      f' When I talk of stuffs I mean writing programs ')
